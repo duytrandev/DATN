cfedbd77d2b19bcc974b254ba68d32fa936c4800
duytran <81101080+duychan@users.noreply.github.com>
2023-06-21 00:47:07 +0700

    dockerize fastapi

--- models/model.py
+++
@@ -1,9 +0,0 @@
-import pickle
-import re
-from pathlib import Path
-BASE_DIR = Path(__file__).resolve(strict=False).parent
-
-model = pickle.load(open("./models/svm.sav", "rb"))
-def predict_svm(input):
-    return model.predict(input)
-
