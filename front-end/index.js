ce545adb5506edbc4217f6f92cb797608469c9b7
Duy Tran <tranlongduydev@gmail.com>
2023-07-10 12:53:21 +0700

    done run model

--- front-end/index.js
+++ front-end/index.js
@@ -2,22 +2,32 @@ const submitButton = document.querySelector(".btn-submit");
 const categoryElement = document.getElementById("label");
 const confidenceElement = document.getElementById("cond");
 const loaderElement = document.getElementById("loader");
-
+const selectItem = document.querySelector(".input-select");
+const result = document.querySelector(".label-choose");
+const content = document.querySelector(".input-text-area");
 submitButton.addEventListener("click", classify);
+selectItem.addEventListener("change", (e) => {
+  categoryElement.textContent = "";
+  confidenceElement.textContent = "";
+});
 
-function classify() {
+function classify(e) {
   submitButton.classList.add("hide-button");
   loaderElement.classList.add("show-loader");
-  fetch("https://jsonplaceholder.typicode.com/todos/1", {
-    method: "GET",
+  const algorithm = selectItem.value;
+  const bodyInput = { input: content.value };
+  fetch(`http://127.0.0.1:8000/predict/${algorithm}/`, {
+    method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
+    body: JSON.stringify(bodyInput),
   })
     .then((response) => response.json())
     .then((data) => {
-      categoryElement.textContent = data.title;
-      confidenceElement.textContent = data.id;
+      console.log(data);
+      categoryElement.textContent = data.label;
+      confidenceElement.textContent = data.conf;
       submitButton.classList.remove("hide-button");
       loaderElement.classList.remove("show-loader");
     })
